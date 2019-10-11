bddac5c3f597a8174107c3a7ecd4ad7a42fca13e
skit <skit@skitnoMacBook-Air.local>
2019-10-12 03:27:49 +0900

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Install plugins
 
+
 ## install dein for nvim
 mkdir -p ~/.cache/dein
 cd ~/.cache/dein
@@ -9,6 +10,8 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/.cache/dein
 
 ## install zgen for zsh
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen
+
+source ~/.zshrc
 
 
