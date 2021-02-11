482bf08142a685aafa96b500f22a8cd260abdc33
aki.f <akihirof0005@gmail.com>
2021-02-11 21:34:09 +0900

    update

--- install.sh
+++ install.sh
@@ -48,34 +48,16 @@ zsh ./installer.sh ~/.cache/dein
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 ## install Rust for *nix
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup component add rls rust-analysis rust-src
-rustup component add rustfmt
+$HOME/dotfiles/install/rust.sh
 
-## install sdk system 
-curl -s "https://get.sdkman.io" | bash
-source ~/.sdkman/bin/sdkman-init.sh
-sdk install java 14.0.2.j9-adpt
-sdk install gradle
-sdk install kotlin
+## install sdk system
+$HOME/dotfiles/install/jvmLan.sh
 
 ## install nodejs
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts
-nvm use --lts
-node -v
+$HOME/dotfiles/install/nodejs.sh
 
 ## install Ruby
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-rbenv --version
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.7.2
-rbenv global 2.7.2
+$HOME/dotfiles/install/ruby.sh
 
 ##swich to zsh
 chsh -s /bin/zsh
@@ -83,4 +65,3 @@ exec zsh
 
 ## some plugins for nvim CocInstall
 `nvim -c "source ~/.bin/cocplugins"`
-
