3067cb505576efaf297bd9481f95a20388864d9c
aki.f <akihirof0005@gmail.com>
2021-02-11 22:18:23 +0900

    update

---
+++ install/systems.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd $HOME/dotfiles/
+git pull
+
+if [ "$(uname)" == 'Darwin' ]; then
+  echo "update system packages"
+  softwareupdate --install --all
+  echo "install brew package manager"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  brew install neovim zip unzip
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  if [ -e /etc/arch-release ]; then
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si
+    cd ~/dotfiles
+    yay -Syu
+    paccache -r
+    paccache -ruk0
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+    cat ~/dotfiles/pkgname/yay.list | yay -S -
+    cat ~/dotfiles/pkgname/pacman.list | yay -S -
+  elif [ -e /etc/lsb-release ]; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+    sudo apt install neovim zsh zip unzip autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev
+  elif [ -e /etc/debian_version ]; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+    sudo apt install neovim zsh zip unzip autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev
+  elif [ "$(expr substr $(uname -m) 1 7)" == 'aarch64' ]; then
+    pkg update
+    pkg upgrade
+    pkg install neovim openssh zsh  zip unzip 
+    termux-setup-storage
+  fi
+fi
+
---
+++ update/systems.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cd $HOME/dotfiles/
+git pull
+
+if [ "$(uname)" == 'Darwin' ]; then
+  echo "update system packages"
+  softwareupdate --install --all
+  echo "update brew packages"
+  brew update
+  brew upgrade
+  echo "update brew cask packages"
+  brew cask upgrade
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  if [ -e /etc/arch-release ]; then
+    yay -Syu;
+    paccache -r;
+    paccache -ruk0
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+    pacman -Qmq > ~/dotfiles/pkgname/yay.list
+    pacman -Qnq > ~/dotfiles/pkgname/pacman.list
+  elif [ -e /etc/lsb-release ]; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+  elif [ -e /etc/debian_version ]; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+  elif [ "$(expr substr $(uname -m) 1 7)" == 'aarch64' ]; then
+    pkg update
+    pkg upgrade
+    termux-setup-storage
+  fi
+fi
+
