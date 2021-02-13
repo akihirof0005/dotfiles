7a0bb448e87e5e4611eb7e12914e689037110ee8
akihiro fujita <akihirof0005@gmail.com>
2021-02-14 06:38:10 +0900

    update

--- install/cocplugin.sh
+++ install/cocplugin.sh
@@ -1,1 +1,1 @@
-nvim -c "CocInstall coc-git coc-html coc-java coc-json coc-rls coc-sh coc-yaml coc-xml coc-yank coc-tsserver coc-texlab coc-solargraph"
+alacritty -e nvim -c "CocInstall coc-git coc-html coc-java coc-json coc-rls coc-sh coc-yaml coc-xml coc-yank coc-tsserver coc-texlab coc-solargraph" &
