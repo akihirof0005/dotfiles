26091b5d9746919ec6c0977fa1d556b9e91e1259
akihiro fujita <akihirof0005@gmail.com>
2021-02-27 14:30:02 +0900

    update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -72,48 +72,42 @@ keysToRemove x =
 strippedKeys x = foldr Data.Map.Strict.delete (keys defaultConfig x) (keysToRemove x)
 
 main :: IO ()
-
 main = xmonad =<< xmobar defaults
-  --wsbar <- spawnPipe myWsBar
---  xmonad $  docks $ ewmh kdeConfig
-  -- xmonad $  docks $ ewmh def
-  -- xmonad =<< xmobar def
-  --xmonad $  docks $ def
-defaults = defaultConfig 
-    { borderWidth = 1
-    , terminal = "alacritty"
-    , normalBorderColor = colorGray
-    , focusedBorderColor = colorGreen
-    , startupHook = myStartupHook
-    , manageHook = placeHook myPlacement <+> myManageHookShift <+> myManageHookFloat <+> manageDocks
-    , layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout
-   -- , logHook = myLogHook wsbar
-    , handleEventHook = fadeWindowsEventHook
-    , workspaces = myWorkspaces
-    , modMask = modm
-    }
-
-    `additionalKeys`
-    [ ((modm , xK_w ), spawn "firefox")
-    , ((modm , xK_f ), spawn "thunar")
-    , ((modm , xK_b ), spawn "xfce4-screenshooter")
-    , ((modm , xK_s ), spawn "slack")
-    , ((modm , xK_i ), spawn "google-chrome-stable --app=chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html")
+defaults = defaultConfig
+      {
+        borderWidth = 1,
+        terminal = "alacritty",
+        normalBorderColor = colorGray,
+        focusedBorderColor = colorGreen,
+        startupHook = myStartupHook,
+        manageHook = placeHook myPlacement <+> myManageHookShift <+> myManageHookFloat <+> manageDocks,
+        layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout,
+   -- , logHook = myLogHook wsbar,
+        handleEventHook = fadeWindowsEventHook,
+        workspaces = myWorkspaces,
+        modMask = modm
+      }
+      `additionalKeys`
+      [ ((modm , xK_w ), spawn "firefox")
+      , ((modm , xK_f ), spawn "thunar")
+      , ((modm , xK_b ), spawn "xfce4-screenshooter")
+      , ((modm , xK_s ), spawn "slack")
+      , ((modm , xK_i ), spawn "google-chrome-stable --app=chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html")
 --    , ((modm , xK_n ), spawn "alacritty -e  nmtui ")
 --    , ((modm , xK_t ), spawn "alacritty -e tw")
-    , ((modm , xK_t ), spawn "tv")
-    , ((modm , xK_z ), spawn "jd")
-    , ((modm , xK_d), windows copyToAll)
-    , ((modm , xK_c ), kill ) -- %! Close the focused window
-    , ((modm , xK_p ), spawn "rofi -show run")
-    , ((modm , xK_a ), spawn "rofi -show ssh")
-    , ((modm , xK_Tab ), moveTo Next NonEmptyWS)
-    , ((modm .|. shiftMask, xK_Tab ), moveTo Prev NonEmptyWS)
-    , ((modm , xK_Up ), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5% && volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
-    , ((modm , xK_Down ), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5% && volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
-    , ((modm , xK_Left ), spawn "headphone")
-    , ((modm , xK_Right ), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
-    ]
+      , ((modm , xK_t ), spawn "tv")
+      , ((modm , xK_z ), spawn "jd")
+      , ((modm , xK_d), windows copyToAll)
+      , ((modm , xK_c ), kill ) -- %! Close the focused window
+      , ((modm , xK_p ), spawn "rofi -show run")
+      , ((modm , xK_a ), spawn "rofi -show ssh")
+      , ((modm , xK_Tab ), moveTo Next NonEmptyWS)
+      , ((modm .|. shiftMask, xK_Tab ), moveTo Prev NonEmptyWS)
+      , ((modm , xK_Up ), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5% && volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
+      , ((modm , xK_Down ), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5% && volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
+      , ((modm , xK_Left ), spawn "headphone")
+      , ((modm , xK_Right ), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle volnoti-show $(amixer get Master | grep -Po \"[0-9]+(?=%)\" | tail -1)")
+      ]
 
 myLayout = withIM (1/5) (ClassName "Pragli") (spacing 3 $  emptyBSP  )
       ||| noBorders Full
