2bd2a919d1200d2d67f0056e78b3f1f53f10778e
aki.f <akihirof0005@gmail.com>
2021-03-29 16:56:53 +0900

    update

--- install/ruby.sh
+++ install/ruby.sh
@@ -3,8 +3,8 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 rbenv --version
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.7.2
-rbenv global 2.7.2
+rbenv install 3.0.0
+rbenv global 3.0.0
 [ -z "$RBENV_ROOT" ] && export RBENV_ROOT="$HOME/.rbenv"
 mkdir -p "$RBENV_ROOT/plugins"
 git clone https://github.com/rkh/rbenv-update.git "$RBENV_ROOT/plugins/rbenv-update"
