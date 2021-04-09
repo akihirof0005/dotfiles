2cad20873b16899fc1dfe96a3f32b657b787bebc
skit <skit@skit.local>
2021-04-09 13:47:21 +0900

    update

--- install/jvmLan.sh
+++ install/jvmLan.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 curl -s "https://get.sdkman.io" | bash
 source ~/.sdkman/bin/sdkman-init.sh
-sdk install java 14.0.2.j9-adpt
+if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(uname -m)" == 'arm64' ]; then
+sdk install java 11.0.10-zulu
+fi
+else
+sdk install java 11.0.10.j9-adpt
+fi
 sdk install gradle
 sdk install kotlin
