52bb8db1c93147a65cf092ce2a6c811fbd5ce852
akihirof0005 <akihirof0005@gmail.com>
2021-06-14 11:22:41 +0900

    update

--- update/dein.sh
+++
@@ -1,4 +0,0 @@
-#cd ~/.cache/dein
-#git pull origin main
-nvim -c "call dein#update() | q"
-echo done!
--- update/zinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-source "$HOME/.zinit/bin/zinit.zsh"
-zinit self-update
-zinit update
-
