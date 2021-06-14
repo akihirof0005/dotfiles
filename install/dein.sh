bd601bd9f104156b18ea6f156896876eca977b8c
akihirof0005 <akihirof0005@gmail.com>
2021-06-14 11:16:39 +0900

    update

--- install/dein.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.cache/dein
-cd ~/.cache/dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-zsh ./installer.sh ~/.cache/dein
-
-cd ~/dotfiles
